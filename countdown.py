7e9b783c6d792c9a0a01419aed5b3cc3041695be
LuQ <lp.wroblewski@gmail.com>
2024-01-21 17:34:16 +0100

    V0.9

--- countdown.py
+++ countdown.py
@@ -1,60 +1,70 @@
-import time
-from tkinter import *
+from tkinter import END
 
 
 class Countdown:
-    def __init__(self, window, minutes_entry, seconds_entry, callback, pauza):
-
-
+    def __init__(self, window, minutes_entry, seconds_entry, progress_bar, callback, pauza):
         self.window_timer = None
+
         self.window = window
         self.minutes_entry = minutes_entry
         self.seconds_entry = seconds_entry
+        self.progress_bar = progress_bar
 
         self.minutes = int(self.minutes_entry.get())
         self.seconds = int(self.seconds_entry.get())
 
+        self.total_seconds = self.minutes * 60 + self.seconds
+        self.remaining_seconds = self.total_seconds
+
         self.callback = callback
         self.pauza = pauza
-
 
     def set_time(self, minutes, seconds):
         self.minutes = minutes
         self.seconds = seconds
+        self.total_seconds = self.minutes * 60 + self.seconds
+        self.remaining_seconds = self.total_seconds
+        self.update_progress_bar()
 
     def update_timer_time(self, minutes=0, seconds=0):
-        self.window.after_cancel(self.window_timer)  # Anuluj poprzednie zdarzenie after
-        self.window_timer = self.window.after(10, self.timer)  # Ustaw nowe zdarzenie after po 10 milisekundach
-        self.time()  # Aktualizuj czas natychmiast
+        self.window.after_cancel(self.window_timer)
+        self.window_timer = self.window.after(10, self.timer)
+        self.time()
 
     def timer(self):
         if not self.pauza:
             self.time()
+            self.update_progress_bar()
 
-            # Sprawdź, czy mamy jeszcze co odliczać
-            if self.seconds > 0 or self.minutes > 0:
+            if self.remaining_seconds > 0:
                 self.window_timer = self.window.after(1000, self.timer)
             else:
                 self.callback()
-
         else:
             return
 
     def time(self):
-        if self.seconds > 0:
-            self.seconds -= 1
+        if self.remaining_seconds > 0:
+            self.remaining_seconds -= 1
 
-        elif self.minutes > 0:
-            self.minutes -= 1
-            self.seconds = 59
+            self.minutes = self.remaining_seconds // 60
+            self.seconds = self.remaining_seconds % 60
 
-        # Sformatuj wartości do postaci z dwiema cyframi
-        formatted_minutes = str(self.minutes).zfill(2)
-        formatted_seconds = str(self.seconds).zfill(2)
+            formatted_minutes = str(self.minutes).zfill(2)
+            formatted_seconds = str(self.seconds).zfill(2)
 
-        # Aktualizuj wartości Entry
-        self.minutes_entry.delete(0, END)
-        self.minutes_entry.insert(END, formatted_minutes)
+            self.minutes_entry.delete(0, END)
+            self.minutes_entry.insert(END, formatted_minutes)
 
-        self.seconds_entry.delete(0, END)
-        self.seconds_entry.insert(END, formatted_seconds)
+            self.seconds_entry.delete(0, END)
+            self.seconds_entry.insert(END, formatted_seconds)
+
+    def update_progress_bar(self):
+        try:
+            progress_percentage = 1 - (self.remaining_seconds / self.total_seconds)
+            self.progress_bar.set(progress_percentage)
+
+        except ZeroDivisionError:
+            self.progress_bar.set(100)
+
+            pass
--- window.py
+++ window.py
@@ -1,11 +1,13 @@
-from tkinter import *
-from countdown import Countdown
+import customtkinter
 import pygame
+from PIL import Image
+from countdown import Countdown
 
 FONT = "Roboto"
 SOUND_CLICK = "assets/sounds/click.mp3"
 SOUND_BREAK = "assets/sounds/break.mp3"
 SOUND_RESET = "assets/sounds/reset.mp3"
+SOUND_ERROR = "assets/sounds/error.mp3"
 
 
 class PomodoroTimerWindow:
@@ -13,7 +15,7 @@ class PomodoroTimerWindow:
 
         self.work_times = 0
         self.checkmark_labels = []
-        self.rely = 0.23
+        self.relx = 0.2
 
         self.starting_work_minutes = None
         self.starting_work_seconds = None
@@ -23,43 +25,62 @@ class PomodoroTimerWindow:
         self.timer_is_on = False
         self.pauza = False
 
-        self.window = Tk()
-        self.window.title("Pomodoro Timer ")
+        self.window = customtkinter.CTk()
+        self.window.title("Pomodoro Timer")
         self.window.minsize(width=500, height=450)
 
-        # Work/Break label
-        self.work_break_label = Label(text="Choose your timer", fg="#000000", font=(FONT, 30, "bold"))
-        self.work_break_label.place(relx=0.5, rely=0.07, anchor=CENTER)
+        self.apperance = customtkinter.set_appearance_mode("dark")
+        self.color_scheem = customtkinter.set_default_color_theme("blue")
 
-        # Tomato image
-        self.tomato_canvas = Canvas(width=200, height=224, borderwidth=0, highlightthickness=0)
-        self.tomato_image = PhotoImage(file="assets/images/tomato.png")
-        self.tomato_canvas.create_image(100, 113, image=self.tomato_image)
-        self.tomato_canvas.place(relx=0.5, rely=0.45, anchor=CENTER)
+        self.frame = customtkinter.CTkFrame(master=self.window)
+        self.frame.pack(padx=25, pady=25, fill="both", expand=True)
+
+        self.second_frame = customtkinter.CTkFrame(master=self.frame, width=250, height=100)
+        self.second_frame.place(relx=0.5, rely=0.25, anchor=customtkinter.CENTER)
+
+        # Work/Break label
+        self.work_break_label = customtkinter.CTkLabel(master=self.frame, text="Choose your timer",
+                                                       font=(FONT, 35, "bold"))
+        self.work_break_label.place(relx=0.5, rely=0.06, anchor=customtkinter.CENTER)
 
         # Hours entry
-        self.minutes = Entry(master=self.tomato_canvas, width=2, borderwidth=0, fg="#FFFFFF", bg="#F26849",
-                             font=(FONT, 30, "bold"))
-        self.minutes.insert(END, string="00")
-        self.minutes.place(relx=0.36, rely=0.57, anchor=CENTER)
+        self.minutes = customtkinter.CTkEntry(master=self.frame,
+                                              width=120,
+                                              justify=customtkinter.CENTER,
+                                              placeholder_text="25",
+                                              font=(FONT, 30, "bold"))
+        self.minutes.insert(customtkinter.END, string="25")
+        self.minutes.place(relx=0.30, rely=0.59, anchor=customtkinter.CENTER)
 
         # Colon label
-        self.colon_label = Label(master=self.tomato_canvas, text=":", fg="#FFFFFF", bg="#F26849",
-                                 font=(FONT, 30, "bold"))
-        self.colon_label.place(relx=0.51, rely=0.57, anchor=CENTER)
+        self.colon_label = customtkinter.CTkLabel(master=self.frame, text=":", font=(FONT, 30, "bold"))
+        self.colon_label.place(relx=0.50, rely=0.59, anchor=customtkinter.CENTER)
 
         # Minutes entry
-        self.seconds = Entry(master=self.tomato_canvas, width=2, borderwidth=0, fg="#FFFFFF", bg="#F26849",
-                             font=(FONT, 30, "bold"))
-        self.seconds.insert(END, string="10")
-        self.seconds.place(relx=0.66, rely=0.57, anchor=CENTER)
+        self.seconds = customtkinter.CTkEntry(master=self.frame,
+                                              width=120,
+                                              justify=customtkinter.CENTER,
+                                              font=(FONT, 30, "bold"))
+        self.seconds.insert(customtkinter.END, string="00")
+        self.seconds.place(relx=0.70, rely=0.59, anchor=customtkinter.CENTER)
 
         # Start_pauza button
-        self.start_button = Button(text="Start", font=(FONT, 20, "bold"),
-                                   command=self.start_button)
-        self.start_button.place(relx=0.5, rely=0.85, anchor=CENTER)
+        self.start_button = customtkinter.CTkButton(master=self.frame,
+                                                    text="Start",
+                                                    width=130,
+                                                    height=45,
+                                                    border_color="#606060",
+                                                    border_width=2,
+                                                    font=(FONT, 30, "bold"),
+                                                    command=self.start_button)
+        self.start_button.place(relx=0.5, rely=0.85, anchor=customtkinter.CENTER)
 
-        self.countdown = Countdown(self.window, self.minutes, self.seconds, self.timer_finished, self.pauza)
+        # Progress_bar
+        self.progress_bar = customtkinter.CTkProgressBar(master=self.frame, height=300, orientation="vertical",)
+        self.progress_bar.place(relx=0.92, rely=0.5, anchor=customtkinter.CENTER)
+
+        self.countdown = Countdown(self.window, self.minutes, self.seconds, self.progress_bar, self.timer_finished,
+                                   self.pauza)
 
     @staticmethod
     def play_sound(sound_path):
@@ -68,33 +89,45 @@ class PomodoroTimerWindow:
         pygame.mixer.music.play()
 
     def start_button(self):
-        if self.timer_is_on:
-            self.timer_is_on = False
-            self.pauza = True
-            self.countdown.pauza = self.pauza
-            self.play_sound(SOUND_BREAK)
-            self.start_button.configure(text="Start")
-            self.next_track_icon_button.destroy()
-            self.work_break_manager()
+        try:
+            minutes = int(self.minutes.get())
+            seconds = int(self.seconds.get())
 
-        else:
-            self.pauza = False
-            self.timer_is_on = True
-            self.countdown.pauza = self.pauza
-            self.start_button.configure(text="Pauza")
-            self.countdown.set_time(int(self.minutes.get()), int(self.seconds.get()))
-            self.play_sound(SOUND_CLICK)
-            self.get_starting_time()
-            self.countdown.timer()
-            self.create_next_track_button()
-            self.work_break_manager()
+            if minutes == 0 and seconds == 0:
+                raise ValueError("Invalid time: Both minutes and seconds cannot be 0.")
+
+            if self.timer_is_on:
+                self.countdown.set_time(minutes, seconds)
+                self.timer_is_on = False
+                self.pauza = True
+                self.countdown.pauza = self.pauza
+                self.play_sound(SOUND_BREAK)
+                self.start_button.configure(text="Start")
+                self.next_track_icon_button.destroy()
+                self.work_break_manager()
+                self.seconds.configure(takefocus=True)
+            else:
+                self.pauza = False
+                self.countdown.set_time(minutes, seconds)
+                self.timer_is_on = True
+                self.countdown.pauza = self.pauza
+                self.start_button.configure(text="Pause")
+                self.get_starting_time()
+                self.play_sound(SOUND_CLICK)
+                self.countdown.timer()
+                self.create_next_track_button()
+                self.work_break_manager()
+
+        except ValueError:
+            self.play_sound(SOUND_ERROR)
+            return
 
     def timer_finished(self):
         self.timer_is_on = False
         self.start_button.configure(text="Start")
         self.pauza = False
         self.countdown.pauza = self.pauza
-        self.work_break_label.config(text="Choose your timer")
+        self.work_break_label.configure(text="Choose your timer")
         self.next_track_icon_button.destroy()
         self.play_sound(SOUND_RESET)
         self.work_times += 1
@@ -111,11 +144,14 @@ class PomodoroTimerWindow:
 
     def create_next_track_button(self):
         if self.timer_is_on:
-            self.next_track_icon = PhotoImage(file="assets/images/next_track_icon.png")
-            self.next_track_icon = self.next_track_icon.subsample(4, 4)
-            self.next_track_icon_button = Button(self.window, image=self.next_track_icon, bd=0,
-                                                 command=self.skip_button)
-            self.next_track_icon_button.place(relx=0.85, rely=0.51, anchor=CENTER)
+            self.next_track_icon_button = customtkinter.CTkButton(master=self.frame, text="⏭️",
+                                                                  command=self.skip_button,
+                                                                  width=45,
+                                                                  height=45,
+                                                                  fg_color="transparent",
+                                                                  hover_color="#606060",
+                                                                  font=(FONT, 35))
+            self.next_track_icon_button.place(relx=0.8, rely=0.85, anchor=customtkinter.CENTER)
 
     def run(self):
         self.window.mainloop()
@@ -123,8 +159,8 @@ class PomodoroTimerWindow:
     def checkmark_manager(self):
         if self.work_times >= 9:
             self.work_times = 0
+            self.relx = 0.2
 
-            self.rely = 0.23
             for label in self.checkmark_labels:
                 label.destroy()
             self.checkmark_labels = []
@@ -138,47 +174,50 @@ class PomodoroTimerWindow:
             self.add_hourglass()
 
     def add_check(self):
-        label = Label(text="✔", fg="#16C60C", font=(FONT, 30, "bold"))
-        label.place(relx=0.10, rely=self.rely, anchor=CENTER)
+        label = customtkinter.CTkLabel(master=self.second_frame, text="✔", font=(FONT, 30, "bold"))
+        label.place(relx=self.relx, rely=0.23, anchor=customtkinter.CENTER)
         self.checkmark_labels.append(label)
 
     def add_hourglass(self):
-        label = Label(text="⌛", fg="#ff0000", font=(FONT, 30, "bold"))
-        label.place(relx=0.20, rely=self.rely, anchor=CENTER)
-        self.rely += 0.10
+        label = customtkinter.CTkLabel(master=self.second_frame, text="⌚", font=(FONT, 30, "bold"))
+        label.place(relx=self.relx, rely=0.73, anchor=customtkinter.CENTER)
+        self.relx += 0.20
         self.checkmark_labels.append(label)
 
     def work_break_manager(self):
         if len(self.checkmark_labels) % 2 == 0:
-            self.work_break_label.config(text="Work")
+            self.work_break_label.configure(text="Work")
+            self.change_color_blue()
 
             if len(self.checkmark_labels) % 2 == 0 and not self.timer_is_on and not self.pauza:
-                self.minutes.delete(0, END)
-                self.minutes.insert(END, str(self.starting_work_minutes).zfill(2))
-                self.seconds.delete(0, END)
-                self.seconds.insert(END, str(self.starting_work_seconds).zfill(2))
+                self.minutes.delete(0, customtkinter.END)
+                self.minutes.insert(customtkinter.END, str(self.starting_work_minutes).zfill(2))
+                self.seconds.delete(0, customtkinter.END)
+                self.seconds.insert(customtkinter.END, str(self.starting_work_seconds).zfill(2))
 
         elif len(self.checkmark_labels) == 7 and not self.timer_is_on and not self.pauza:
-            self.work_break_label.config(text="Long Break")
-            if self.starting_break_minutes is not None and self.starting_break_seconds is not None:
+            self.work_break_label.configure(text="Long Break")
+            self.change_color_purple()
 
+            if self.starting_break_minutes is not None and self.starting_break_seconds is not None:
                 long_break_minutes = self.starting_break_minutes * 3
 
-                self.minutes.delete(0, END)
-                self.minutes.insert(END, str(long_break_minutes).zfill(2))
+                self.minutes.delete(0, customtkinter.END)
+                self.minutes.insert(customtkinter.END, str(long_break_minutes).zfill(2))
 
-                self.seconds.delete(0, END)
-                self.seconds.insert(END, str(self.starting_break_seconds).zfill(2))
+                self.seconds.delete(0, customtkinter.END)
+                self.seconds.insert(customtkinter.END, str(self.starting_break_seconds).zfill(2))
 
         elif len(self.checkmark_labels) % 2 == 1 and not self.timer_is_on and not self.pauza:
-            self.work_break_label.config(text="Break")
+            self.work_break_label.configure(text="Break")
+            self.change_color_green()
 
             if self.starting_break_minutes is not None and self.starting_break_seconds is not None:
-                self.minutes.delete(0, END)
-                self.minutes.insert(END, str(self.starting_break_minutes).zfill(2))
+                self.minutes.delete(0, customtkinter.END)
+                self.minutes.insert(customtkinter.END, str(self.starting_break_minutes).zfill(2))
 
-                self.seconds.delete(0, END)
-                self.seconds.insert(END, str(self.starting_break_seconds).zfill(2))
+                self.seconds.delete(0, customtkinter.END)
+                self.seconds.insert(customtkinter.END, str(self.starting_break_seconds).zfill(2))
 
     def get_starting_time(self):
         if (len(self.checkmark_labels) == 0 and self.starting_work_minutes
@@ -190,3 +229,19 @@ class PomodoroTimerWindow:
               is None and self.starting_break_seconds is None):
             self.starting_break_minutes = int(self.minutes.get())
             self.starting_break_seconds = int(self.seconds.get())
+
+    def change_color_blue(self):
+        self.work_break_label.configure(text_color="#1f6aa5")
+        self.start_button.configure(fg_color="#1f6aa5", hover_color="#144870")
+        self.progress_bar.configure(progress_color="#1f6aa5")
+
+    def change_color_green(self):
+        self.work_break_label.configure(text_color="#2ca474")
+        self.start_button.configure(fg_color="#2ca474", hover_color="#106a43")
+        self.progress_bar.configure(progress_color="#2ca474")
+
+    def change_color_purple(self):
+        self.work_break_label.configure(text_color="#6d2c7e")
+        self.start_button.configure(fg_color="#6d2c7e", hover_color="#5d1470")
+        self.progress_bar.configure(progress_color="#6d2c7e")
+
